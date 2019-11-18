e36c2e1978f691ab76b71a58709422a29ab8149a
scarlettperry <si.perry15@gmail.com>
2019-11-17 19:18:52 -0800

    Add work section

---
+++ src/pages/work.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default () => (
+    <div>
+        <div>
+            <h2>Professional Expereince</h2>
+            <div>
+                <h4>Software Engineering Apprentice, Lyft</h4>
+                <p>San Francisco, CA</p>
+                <p>Transit Team</p>
+            </div>
+            <div>
+                <h4>Solutions Engineer, TripleLift</h4>
+                <p>New York, NY</p>
+                <p>Application Reliability Team</p>
+            </div>
+        </div>
+        <div>
+            <h2>Outreach</h2>
+            <div>
+                <h4>CodeNation</h4>
+                <p>San Francisco, CA</p>
+                <p>Ran student mock interviews for Professional Development Day </p>
+            </div>
+            <div>
+                <h4>Write/Speak/Code</h4>
+                <p>San Francisco, CA</p>
+                <p>Provided resume and portfolio reviews at Git Hired</p>
+            </div>
+            <div>
+                <h4>Flatiron School</h4>
+                <p>New York, NY</p>
+                <p>Speaker on the Women Trailblazers in Tech panel</p>
+                <p>Speaker on the Alumni: Ask Me Anything panel</p>
+            </div>
+        </div>
+    </div>
+);
