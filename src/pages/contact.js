6a017f1558b56d967d9d37c7f33a8e0606f45b13
scarlettperry <si.perry15@gmail.com>
2019-11-19 21:17:13 -0800

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default () => (
+    <>
+        <p>Get in touch!</p>
+        <p>si.perry15@gmail.com</p>
+        <ul>
+            <li>
+                <a href="https://twitter.com/scarlettp3rry">
+                    Twitter
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/scarlettperry/">
+                    LinkedIn
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/scarlettperry">
+                    Github
+                </a>
+            </li>
+        </ul>
+    </>
+);
